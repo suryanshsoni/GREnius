4d3b20136cec3bf9be975a7bffb51aa2cce608b8
suryanshsoni <soni.suryansh11130@gmail.com>
2017-07-03 17:23:57 +0530

    drawer added

--- src/main/java/com/tensai/grenius/ui/home/HomeActivity.java
+++ src/main/java/com/tensai/grenius/ui/home/HomeActivity.java
@@ -2,20 +2,17 @@ package com.tensai.grenius.ui.home;
 
 import android.content.Context;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
-import android.view.View;
-import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -23,7 +20,6 @@ import com.tensai.grenius.R;
 import com.tensai.grenius.ui.base.BaseActivity;
 import com.tensai.grenius.ui.home.articles_fragment.ArticlesFragment;
 import com.tensai.grenius.ui.home.dashboard_fragment.DashboardFragment;
-import com.tensai.grenius.ui.home.profile_fragment.ProfileFragment;
 import com.tensai.grenius.ui.home.quiz_fragment.QuizFragment;
 import com.tensai.grenius.ui.home.settings_fragment.SettingsFragment;
 import com.tensai.grenius.ui.home.words_all_fragment.WordsAllFragment;
@@ -32,40 +28,62 @@ import com.tensai.grenius.ui.home.words_synonym_fragement.WordsSynonymFragment;
 
 import javax.inject.Inject;
 
-public class HomeActivity extends BaseActivity implements HomeView, NavigationView.OnNavigationItemSelectedListener,DashboardFragment.OnFragmentInteractionListener,QuizFragment.OnFragmentInteractionListener
-,WordsAllFragment.OnFragmentInteractionListener{
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class HomeActivity extends BaseActivity implements HomeView, DashboardFragment.OnFragmentInteractionListener, QuizFragment.OnFragmentInteractionListener
+        , WordsAllFragment.OnFragmentInteractionListener {
 
     @Inject
     HomePresenter<HomeView> presenter;
+    @BindView(R.id.bottom_navigation)
+    BottomNavigationView bottomNavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        ButterKnife.bind(this);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
 
+        bottomNavigation.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        Log.d("MENU", item.getItemId() + "|Item id");
+                        switch (item.getItemId()) {
+                            case R.id.nav_words:
+                                WordsAllFragment fragment = WordsAllFragment.getInstance();
+                                getSupportFragmentManager().beginTransaction().replace(R.id.mainFrame, fragment)
+                                        .commit();
+                                break;
+                            case R.id.nav_categories:
+                                //showFragment(WordsSynonymFragment.class);
+                                break;
+
+                            case R.id.nav_home:
+                                showFragment(DashboardFragment.class);
+                                break;
+
+                            case R.id.nav_quiz:
+                                showFragment(QuizFragment.class);
+                                break;
+
+                            case R.id.nav_articles:
+                                showFragment(ArticlesFragment.class);
+                                break;
+                        }
+                        return true;
+                    }
+                }
+        );
         showFragment(DashboardFragment.class);
     }
 
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
-    }
+
 
 
     @Override
@@ -90,43 +108,14 @@ public class HomeActivity extends BaseActivity implements HomeView, NavigationVi
         return super.onOptionsItemSelected(item);
     }
 
-    public static Intent getStartIntent(Context context){
-        return new Intent(context,HomeActivity.class);
-    }
-
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_words_all) {
-            WordsAllFragment fragment = WordsAllFragment.getInstance();
-            getSupportFragmentManager().beginTransaction().replace(R.id.mainFrame,fragment)
-                    .commit();
-        } else if (id == R.id.nav_words_frequncy) {
-            showFragment(WordsHighFreqFragment.class);
-        } else if (id == R.id.nav_words_synonyms) {
-            showFragment(WordsSynonymFragment.class);
-
-        } else if (id == R.id.nav_quiz) {
-            showFragment(QuizFragment.class);
-
-        } else if (id == R.id.nav_articles) {
-            showFragment(ArticlesFragment.class);
-        } else if (id== R.id.nav_settings){
-            showFragment(SettingsFragment.class);
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
+    public static Intent getStartIntent(Context context) {
+        return new Intent(context, HomeActivity.class);
     }
 
     private void showFragment(Class fragmentClass) {
         Fragment fragment = null;
         try {
-            fragment = (Fragment)fragmentClass.newInstance();
+            fragment = (Fragment) fragmentClass.newInstance();
         } catch (InstantiationException e) {
             e.printStackTrace();
         } catch (IllegalAccessException e) {
@@ -135,7 +124,7 @@ public class HomeActivity extends BaseActivity implements HomeView, NavigationVi
 
         FragmentManager fragmentmanager = getSupportFragmentManager();
         fragmentmanager.beginTransaction()
-                .replace(R.id.mainFrame,fragment)
+                .replace(R.id.mainFrame, fragment)
                 .commit();
     }
 
